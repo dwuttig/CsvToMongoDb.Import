d34431855fad1635176e113c2d78f6e357590071
Daniel Wuttig <d.wuttig@gmx.de>
2024-09-11 18:13:00 +0200

    rename collection to actual block id

--- CsvToMongoDb.Import/ImportService.cs
+++ CsvToMongoDb.Import/ImportService.cs
@@ -43,6 +43,6 @@ public class ImportService : IImportService
         }
 
         var blockNr = _repository.SearchDocument("Name", "BlockNr", collectionName);
-        _repository.RenameCollection(collectionName, blockNr.Name);
+        _repository.RenameCollection(collectionName, blockNr.Value);
     }
 }
